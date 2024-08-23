e1876e0f58761b1112b9358ada5b0025124505e5
YaroslavPC <kaawaabanga@gmail.com>
2024-08-23 14:53:51 +0300

    full_screen

--- ui/ui.gd
+++ ui/ui.gd
@@ -1,5 +1,6 @@
 extends Control
 
+
 @onready var menu: Window = $menu_win;
 @onready var settingsBtn: Button = $settings_btn;
 @onready var fullModeCbtn: CheckButton = $menu_win/menu_top_vbox/full_mode_cbtn;
@@ -16,7 +17,7 @@ func _process(delta: float) -> void:
 
 
 func connect_controls():
-	fullModeCbtn.connect('pressed', _on_full_mode_cbtn_pressed);
+	fullModeCbtn.connect('toggled', _on_full_mode_cbtn_pressed);
 	settingsBtn.connect('pressed', toggle_menu.bind(true));
 	exitBtn.connect('pressed', _on_exit_btn_pressed);
 	menu.connect('close_requested', toggle_menu.bind(false))
@@ -29,8 +30,11 @@ func toggle_menu(_visible: bool = false) -> void:
 		menu.hide();
 
 
-func _on_full_mode_cbtn_pressed() -> void:
-	print('_on_full_mode_cbtn_pressed');
+func _on_full_mode_cbtn_pressed(toggled: bool = false) -> void:
+		if toggled:
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
+		else:
+			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_WINDOWED)
 
 
 func _on_exit_btn_pressed() -> void:
